5fc577933388811ad327550721ae58d9f47257f0
Beev37 <brendanvarilek@gmail.com>
2022-01-31 01:44:05 -0800

    start of hw1

---
+++ hw_01/student_info.py
@@ -0,0 +1,3 @@
+output_stuff = ["Name:", "Birthday:", "Hobbies:", "Favorite Book:", "Favorite Movie"]
+brendan = ["Brendan Varilek", "August 9th 1995", "Cooking, Making Music, and Playing Games", "Neuromancer", "The Lord of the Rings Trilogy"]
+

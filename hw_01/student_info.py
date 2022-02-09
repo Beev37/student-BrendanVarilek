36ecb67b21ce379073908a0fca22e5955f47a285
Brendan Varilek <varilek.b@northeastern.edu>
2022-02-09 15:10:07 -0800

    working on hw1 & testing workflow w other cpu

--- hw_01/student_info.py
+++ hw_01/student_info.py
@@ -1,3 +1,13 @@
-output_stuff = ["Name:", "Birthday:", "Hobbies:", "Favorite Book:", "Favorite Movie"]
+gnrl_traits = ["Name:", "Birthday:", "Hobbies:", "Favorite Book:", "Favorite Movie"]
 brendan = ["Brendan Varilek", "August 9th 1995", "Cooking, Making Music, and Playing Games", "Neuromancer", "The Lord of the Rings Trilogy"]
 
+prsn_traits = {}
+ppl_traits = {}
+
+for x in gnrl_traits:
+    prsn_traits[x] = brendan[gnrl_traits.index(x)]
+
+#print(prsn_traits)
+
+for pair in prsn_traits.items():
+    print(pair)

14f8ad6a878e4d2f005efdb832cc34627e0308b1
Brendan Varilek <varilek.b@northeastern.edu>
2022-02-11 15:32:19 -0800

    building framework for heart rate calculations

---
+++ hw_02/training_zones.py
@@ -0,0 +1,10 @@
+age = input("Please enter your age: ")
+resting_hr = input("Please enter your resting heart rate: ")
+print("===============================================")
+
+max_heart_rate = 208 - (0.7 * age)
+relative_hr_percentage = 100(current_heart_rate/max_heart_rate)
+
+
+print(f"Your heart rate reserve is: {hr_reserve} bpm")
+print("Here is a breakdown of your training zones: ")

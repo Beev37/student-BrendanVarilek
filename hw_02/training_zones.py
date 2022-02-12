7d4ec4273856cb0c69174b7322f9edc20b85935a
Brendan Varilek <varilek.b@northeastern.edu>
2022-02-11 17:03:47 -0800

    printed zone ranges in proper format. ran into trouble with the range boundaries being 0.01 different from each other, and noticed something interesting regarding this trouble -it's commented in line 14 here

--- hw_02/training_zones.py
+++ hw_02/training_zones.py
@@ -10,14 +10,21 @@ hr_reserve = float(max_heart_rate - resting_hr)
 hr_zones = {}
 zone_num = 0
 perc_low = .4
-perc_high = .5
+perc_high = .4999
+#interesting thing happened w/ "perc_high" value. if it is set to .499 (one less 9 than current value), the resulting boundaries between ranges are grater than 0.01,
+#and if you set that value to 0.49999 (one more 9 than current value), then the boundaries between ranges are non-existent. i'm very curious. 
 
-while zone_num <= 5:
+while zone_num <= 4:
     zone_num += 1
     perc_low += .1
     perc_high += .1
-    hr_zones[zone_num] = (round(float((resting_hr + (hr_reserve*perc_low))), 3), round(float((resting_hr + (hr_reserve*perc_high))), 3))
+    hr_zones[zone_num] = (round(float((resting_hr + (hr_reserve*perc_low))), 2), round(float((resting_hr + (hr_reserve*perc_high))), 2))
 
-print(f"Your heart rate reserve is: {round(hr_reserve, 3)} bpm")
-
+print(f"Your heart rate reserve is: {round(hr_reserve, 2)} bpm")
 print("Here is a breakdown of your training zones: ")
+zone_num = 1
+hr_index = 1
+for zone_range in range(len(hr_zones)):
+    print(f"Zone {zone_num}: {hr_zones[hr_index][0]} to {hr_zones[hr_index][1]} bpm")
+    zone_num += 1
+    hr_index += 1

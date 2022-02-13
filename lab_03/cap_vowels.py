5fafe77c0be705a8541813cc32640c66e266feb4
Beev37 <brendanvarilek@gmail.com>
2022-02-13 15:14:32 -0800

    test

--- lab_03/cap_vowels.py
+++ lab_03/cap_vowels.py
@@ -1,4 +1,15 @@
-vowels = ['a','e','i','o','u']
+"""This code propmts the user to enter a sentence, and the output will result in the same
+sentence but with the vowels capitalized, and consonants in lower case"""
 
 def case_changer(string):
-    
+    """function that takes strings and outputs a new string with the desired modifications"""
+    vowels = ['a','e','i','o','u']
+    new_string = ""
+    for chrctr in string:
+        if chrctr not in vowels:
+            chrctr = chrctr.lower()
+        elif chrctr in vowels:
+            chrctr = chrctr.upper()
+        new_string = new_string + chrctr
+
+    return new_string

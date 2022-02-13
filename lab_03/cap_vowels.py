e492283c8520b6cdb6f0126d4efed94870e3cc4c
Beev37 <brendanvarilek@gmail.com>
2022-02-13 15:47:02 -0800

    output registers capital inputs incorrectly. adjusting.

--- lab_03/cap_vowels.py
+++ lab_03/cap_vowels.py
@@ -6,10 +6,17 @@ def case_changer(string):
     vowels = ['a','e','i','o','u']
     new_string = ""
     for chrctr in string:
+        low_chrctr = chrctr.lower()
+        high_chrctr = chrctr.upper()
         if chrctr not in vowels:
             chrctr = chrctr.lower()
+        
         elif chrctr in vowels:
             chrctr = chrctr.upper()
         new_string = new_string + chrctr
 
+    print(new_string)
     return new_string
+
+usr_in = input("Enter a sentence to be modified: ")
+case_changer(usr_in)
